cdf3501b85648ccb7369d3a1a1f32a464d4bc18e
Matthew Foyle <mfoyle93@gmail.com>
2023-07-19 10:02:11 +0200

    feat: add example component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 'use client'
+import { useDynamicContext } from "@dynamic-labs/sdk-react";
+
 import InstructionsComponent from "@/components/instructionsComponent";
+import NftGallery from "@/components/nftGalleryComponent";
 import styles from "./page.module.css";
 import "./globals.css";
 
 export default function Home() {
+  const { primaryWallet } = useDynamicContext();
+  const address = primaryWallet?.address;
+
   return (
     <main className={styles.main}>
+      <NftGallery pageSize={5} walletAddress={address} collectionAddress={"0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"} chain={"ETH_MAINNET"}></NftGallery>
       <InstructionsComponent></InstructionsComponent>
     </main>
   );
--- components/instructionsComponent/navigation/navbar/index.tsx
+++ components/instructionsComponent/navigation/navbar/index.tsx
@@ -1,8 +1,8 @@
-
 'use client'
+
 import {DynamicWidget } from "@dynamic-labs/sdk-react";
 
-import styles from "./Navbar.module.css";
+import styles from "./navbar.module.css";
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
